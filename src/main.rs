725b4286cb96f095c8934a514560c262e49669a2
Lukas Panneke <l.panneke@googlemail.com>
2023-07-25 19:48:54 +0200

    Avoid unnecessary allocations

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,46 @@
-use chrono::{Duration, NaiveDateTime};
-use evtclib::{Compression, Log, Outcome};
-use std::env;
+use std::io::Write;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
+    let file_path: String = std::env::args()
+        .nth(1)
+        .ok_or("First (and only) argument must be the path to a zevtc file")?;
+    let log = evtclib::process_file(file_path, evtclib::Compression::Zip)?;
+    let mut out = std::io::BufWriter::new(std::io::stdout().lock());
 
-    let file = &args[1];
-    let log: Log = evtclib::process_file(file, Compression::Zip)?;
-    let date = log
-        .local_end_timestamp()
-        .and_then(|unix_timestamp| NaiveDateTime::from_timestamp_opt(unix_timestamp.into(), 0));
-    let encounter = log
-        .encounter()
-        .map(|e| e.to_string())
-        .unwrap_or_else(|| "unknown".to_string());
-    let outcome = log
-        .analyzer()
-        .and_then(|a| a.outcome())
-        .map(|o| match o {
-            Outcome::Success => "kill",
-            Outcome::Failure => "wipe",
-        })
-        .unwrap_or("crash");
-    let duration =
-        humantime::Duration::from(Duration::milliseconds(log.span() as i64).to_std().unwrap());
-    println!(
-        "{} {} after {} on {}",
-        encounter,
-        outcome,
-        duration,
-        date.map_or_else(
-            || "invalid date".to_string(),
-            |t| format!("{}", t.format("%a %e %b %Y, %H:%M"))
-        )
-    );
+    match log.encounter() {
+        Some(encounter) => write!(out, "{encounter} ")?,
+        None => write!(out, "encounter {} ", log.encounter_id())?,
+    }
+
+    let outcome = match log.analyzer().and_then(|a| a.outcome()) {
+        Some(evtclib::Outcome::Success) => "kill",
+        Some(evtclib::Outcome::Failure) => "wipe",
+        None => "crash",
+    };
+    write!(out, "{outcome}")?;
+
+    let duration = humantime::Duration::from(std::time::Duration::from_millis(log.span()));
+    write!(out, " after {duration} ")?;
+
+    if let Some(timestamp) = log.local_end_timestamp().and_then(|unix_timestamp| {
+        chrono::NaiveDateTime::from_timestamp_opt(unix_timestamp.into(), 0)
+    }) {
+        writeln!(out, "on {}", timestamp.format("%a %e %b %Y, %H:%M"))?;
+    }
 
     for player in log.players() {
-        println!(
-            "  {:2} {:22} {:19} {:12}",
-            player.subgroup(),
-            player.account_name(),
-            player.character_name(),
-            player
-                .elite()
-                .map(|e| e.to_string())
-                .unwrap_or_else(|| player.profession().to_string()),
-        )
+        write!(
+            out,
+            "  {subgroup:2} {account:22} {character:19}",
+            subgroup = player.subgroup(),
+            account = player.account_name(),
+            character = player.character_name(),
+        )?;
+        match player.elite() {
+            Some(elite) => writeln!(out, " {elite:12}"),
+            None => writeln!(out, " {:12}", player.profession()),
+        }?;
     }
+    out.flush()?;
     Ok(())
 }
